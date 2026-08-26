f299b443ab80f63627f02b7343186ab9fd691085
ywkim <ywkim@jikyung.com>
2026-08-26 15:56:42 +0900

    feat: 게시글 작성 POST 처리 및 DB 저장 기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -1,14 +1,32 @@
 package com.study.board.controller;
 
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private final BoardService boardService;
+
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
     @GetMapping("/board/write") // localhost:8080/board/write
     public String boardWriteForm(){
+
         return "boardwrite";
-        // return 쌍따옴표 안에 들어가는 것은어떤 html 파일(view 파일)로 보내줄 것인지
+    }
+
+    @PostMapping("/board/writepro")
+    public Board boardWritePro(Board board){
+        boardService.write(board);
+
+        return board;
     }
 }
---
+++ src/main/java/com/study/board/entity/Board.java
@@ -0,0 +1,18 @@
+package com.study.board.entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Board{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+    private String title;
+    private  String content;
+}
