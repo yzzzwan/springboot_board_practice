30833c1d44e48d9900d9130d2938d8567efcc481
yzzzwan <kyw011012@gmail.com>
2026-09-15 00:43:20 +0900

    feat: 파일 업로드

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class BoardController {
@@ -26,8 +27,8 @@ public class BoardController {
     }
 
     @PostMapping("/board/writepro")
-    public String boardWritePro(Board board, Model model){
-        boardService.boardWrite(board);
+    public String boardWritePro(Board board, Model model, MultipartFile file)throws Exception{
+        boardService.boardWrite(board, file);
 
         model.addAttribute("message", "글 작성이 완료되었습니다.");
         model.addAttribute("searchUrl", "/board/list");
@@ -66,12 +67,13 @@ public class BoardController {
     @PostMapping("/board/update/{id}")
     public String boardUpdate(@PathVariable("id") Integer id,
                               Board board,
-                              Model model){
+                              Model model,
+                              MultipartFile file) throws Exception{
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
-        boardService.boardWrite(boardTemp);
+        boardService.boardWrite(boardTemp, file);
 
         model.addAttribute("message", "글이 수정되었습니다.");
         model.addAttribute("searchUrl", "/board/list");
--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -4,8 +4,11 @@ import com.study.board.entity.Board;
 import com.study.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -13,7 +16,17 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     // 게시글 작성
-    public void boardWrite(Board board){
+    public void boardWrite(Board board, MultipartFile file) throws Exception{
+                            // 프로젝트의 root 디렉토리
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+        UUID uuid = UUID.randomUUID();
+        String fileName = uuid + "_" + file.getOriginalFilename();
+        File saveFile = new File(projectPath,fileName);
+        file.transferTo(saveFile);
+
+        board.setFilename(fileName);
+        board.setFilepath("/files/" + fileName);
+
         boardRepository.save(board);
     }
 
